d94a4e61d400589aa5e9974da938d29cb513bfeb
Matthis Michalek <matthiasmichalek@gmail.com>
2023-12-13 10:49:07 +0100

    Initiate Repo

---
+++ src/Dart.jl
@@ -0,0 +1,78 @@
+module Dart
+
+export main,reset_plot
+
+using Plots
+using LinearAlgebra
+
+include("plots.jl")
+
+
+function area_circle_segment(ri,ro,dphi)
+    return dphi * (ro^2 - ri^2)
+end
+
+function center_of_mass(v_rrphi,dphi)
+    ri,ro,phi = v_rrphi    
+    return 2/3 * 1/dphi * (ro^3-ri^3) / (ro^2-ri^2) * [sin(dphi+phi)-sin(phi), -cos(dphi+phi)+cos(phi)]
+end
+
+function calc_coms(rrphi,dphi)
+    return center_of_mass.(rrphi,dphi)        
+end
+
+function area_of_segment(v_rrphi,dphi)
+    ri,ro,phi = v_rrphi
+    return dphi /2 * (ro^2-ri^2)
+end
+
+function areas_of_segments(rrphi,dphi)
+    areas = area_of_segment.(rrphi,dphi)
+    return areas
+end
+
+function weights_of_segments(areas, vals)
+    factors = [1,3,1,2]
+    return areas .* vals .* factors'
+end
+
+function main()
+    rb_i = 6.35
+    rb_o = 16.0
+
+    ri_i = 99.0
+    ri_o = 107.0
+
+    ro_i = 162.0
+    ro_o = 170.0
+
+    vals =[50,25,6,13,4,18,1,20,5,12,9,14,11,8,16,7,19,3,17,2,15,10]
+
+    rs = [rb_i, rb_o, ri_i, ri_o, ro_i, ro_o]
+    
+    dphi = 2π/20
+    phis = LinRange(0,2π-dphi,20) .- dphi/2
+
+    rrphi = [[rs[j+1],rs[j+2],phi] for phi in phis, j in 1:4] 
+
+    coms = calc_coms(rrphi,dphi)
+    areas = areas_of_segments(rrphi,dphi)
+    weights = weights_of_segments(areas,vals[3:end])
+
+    weights_be = [rb_i^2*π*vals[1], (rb_o^2-rb_i^2)*π*vals[2]]
+    mass = sum(weights) + sum(weights_be)
+
+    center_of_mass = sum(coms .* weights)/mass
+
+
+    reset_plot()
+    #Plots.scatter!([0],[0],label="")
+    draw_board(rs,phis)
+    #draw_coms(coms)
+    draw_annotation(200,phis,dphi,vals[3:end])
+    Plots.scatter!([center_of_mass[1]],[center_of_mass[2]])
+
+end
+
+
+end # of module
---
+++ theory.md
@@ -0,0 +1,18 @@
+
+Kreisausschnitt:\
+$r_1$ ... inner radius \
+$r_1$ ... outer radius \
+$\phi$ ... öffnungswinkel
+
+
+
+
+geom. Schwerpunkt:
+
+$\dfrac{2}{3} \; \dfrac{r_1^3-r_0^3}{r_1^2-r_0^2} \; \dfrac{1}{\phi} \cdot \begin{pmatrix} \sin(\phi) \\ 1-\cos(\phi) \end{pmatrix}$ 
+
+
+
+
+
+
